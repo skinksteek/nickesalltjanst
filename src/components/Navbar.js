f4eeb6064761128d0081587792c7f77f09a8ab52
Linus <linusigelstrom@gmail.com>
2024-03-07 19:03:57 +0100

    navbar styling & media queries

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,72 @@
-import '../assets/styles/navbar.css'
-import { Component } from 'react'
-import React from 'react';
+import "../assets/styles/navbar.css";
+import "../assets/styles/styles.css";
+import { Component } from "react";
+import React from "react";
 
 class Navbar extends Component {
-    state={clicked: false}
-    handleClick = () => {
-        this.setState({clicked: !this.state.clicked})
-    }
-    render() {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
+  render() {
     return (
-        <header>
+      <header>
         <nav className="nav">
-            <div className=''>
-                <a id='logo' href='#'>
-                    <img src={require('../assets/images/logo-trans.png')} alt="Nickesalltjänst" />
+          <div className="">
+            <a id="logo" href="/hem">
+              <img
+                src={require("../assets/images/logo-trans.png")}
+                alt="Nickesalltjänst"
+              />
+            </a>
+          </div>
+
+          <div
+            className={this.state.clicked ? "dark-overlay" : "light-overlay"}
+          >
+            <ul
+              id="navbar"
+              className={this.state.clicked ? "#navbar active" : "#navbar"}
+            >
+              <li>
+                <a className="active hover-underline" href="/hem">
+                  Hem
                 </a>
-            </div>
-
-            <div>
-            <ul id="navbar" className={this.state.clicked ? "#navbar active" : "#navbar"}>
-                <li>
-                    <a className="active hover-underline" href="/hem">Hem</a>
-                </li>
-                <li>
-                    <a className='hover-underline' href="/nyheter">Nyheter</a>
-                </li>
-                <li>
-                    <a className='hover-underline' href="/om">Om</a>
-                </li>
-                <li>
-                    <a className='hover-underline' href="/kontakt">Kontakt</a>
-                </li>
+              </li>
+              <li>
+                <a className="hover-underline" href="/nyheter">
+                  Nyheter
+                </a>
+              </li>
+              <li>
+                <a className="hover-underline" href="/om">
+                  Om
+                </a>
+              </li>
+              <li>
+                <a className="hover-underline" href="/kontakt">
+                  Kontakt
+                </a>
+              </li>
             </ul>
-            </div>
-            
+          </div>
 
-            <div id="mobile" onClick={this.handleClick}>
-            <i id="bar" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-            </div>
+          <div
+            id="mobile"
+            onClick={() => {
+              this.handleClick();
+            }}
+          >
+            <i
+              id="bar"
+              className={this.state.clicked ? "fas fa-times " : "fas fa-bars"}
+            ></i>
+          </div>
         </nav>
-        </header>
-    )
-}
+      </header>
+    );
+  }
 }
 
 export default Navbar;
