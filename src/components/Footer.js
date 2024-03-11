bd57cb7a043190a88eb73667741d57dd34d1fca3
Linus <linusigelstrom@gmail.com>
2024-03-11 21:42:18 +0100

    some navbar modifications

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,45 +1,58 @@
 /* eslint-disable no-undef */
-import React from 'react';
-import '../assets/styles/footer.css'
+import React from "react";
+import "../assets/styles/footer.css";
 
 const Footer = () => {
   return (
-    <div className='footer-parent'>
-    <footer className="footer-container">
-      <div className='footer-info footer-50'>
-        <p><strong>Vår vision</strong></p>
-        <p>
-        What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer
-        </p>
-      </div>
-      <div className='footer-contact footer-25'>
-        <p><strong>Kontakta oss</strong></p>
-        <p>
-          Nickesalltjänst AB
-          <br></br>
-          Åbyvägen 4B
-          <br></br>
-          431 62 Mölndal
-          <br></br>
-          <br></br>
-          070-278 58 81
-          <br></br>
-          info@nickesalltjanst.se
-        </p>
-      </div>
-      <div className='footer-follow footer-25'>
-        <ul>
-        <p><strong>Följ oss</strong></p>
-        <li><a href="#">LinkedIn</a></li>
-        <br></br>
-        <li><a href="#">Facebook</a></li>
-        <br></br>
-        <li><a href="#">Instagram</a></li>
-      </ul>
-      </div>
-      
-
-    </footer>
+    <div className="footer-parent">
+      <footer className="footer-container">
+        <div className="footer-info footer-50">
+          <p>
+            <strong>Vår vision</strong>
+          </p>
+          <p>
+            What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry. Lorem Ipsum has been the
+            industrys standard dummy text ever since the 1500s, when an unknown
+            printer
+          </p>
+        </div>
+        <div className="footer-contact footer-25">
+          <p>
+            <strong>Kontakta oss</strong>
+          </p>
+          <p>
+            Nickesalltjänst AB
+            <br></br>
+            Åbyvägen 4B
+            <br></br>
+            431 62 Mölndal
+            <br></br>
+            <br></br>
+            070-278 58 81
+            <br></br>
+            info@nickesalltjanst.se
+          </p>
+        </div>
+        <div className="footer-follow footer-25">
+          <ul>
+            <p>
+              <strong>Följ oss</strong>
+            </p>
+            <li>
+              <a href="#">LinkedIn</a>
+            </li>
+            <br></br>
+            <li>
+              <a href="#">Facebook</a>
+            </li>
+            <br></br>
+            <li>
+              <a href="#">Instagram</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,82 +1,66 @@
 import "../assets/styles/navbar.css";
 import "../assets/styles/styles.css";
-import { Component } from "react";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-class Navbar extends Component {
-  state = { clicked: false };
-  handleClick = () => {
-    this.setState({ clicked: !this.state.clicked });
+export default function Navbar() {
+  const [clicked, setClicked] = useState(false);
+  const [scroll, setScroll] = useState(false);
+  const handleClick = () => {
+    setClicked(!clicked);
   };
-  /*
+
   useEffect(() => {
-    const handleScroll = () => {
-      setSticky(window.scrollY > 200);
-      console.log(window.scrollY);)
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 10);
+    });
   });
-  
-*/
-  render() {
-    return (
-      <header>
-        <nav className="nav">
-          <div className="">
-            <a id="logo" href="/hem">
-              <img
-                src={require("../assets/images/logo-trans.png")}
-                alt="Nickesalltjänst"
-              />
-            </a>
-          </div>
 
-          <div
-            className={this.state.clicked ? "dark-overlay" : "light-overlay"}
-          >
-            <ul
-              id="navbar"
-              className={this.state.clicked ? "#navbar active" : "#navbar"}
-            >
-              <li>
-                <a className="active hover-underline" href="/hem">
-                  Hem
-                </a>
-              </li>
-              <li>
-                <a className="hover-underline" href="/nyheter">
-                  Nyheter
-                </a>
-              </li>
-              <li>
-                <a className="hover-underline" href="/om">
-                  Om
-                </a>
-              </li>
-              <li>
-                <a className="hover-underline" href="/kontakt">
-                  Kontakt
-                </a>
-              </li>
-            </ul>
-          </div>
+  return (
+    <header className={`${scroll ? "sticky-navbar" : ""}`}>
+      <nav className="nav">
+        <div className="">
+          <a id="logo" href="/hem">
+            <img
+              src={require("../assets/images/logo-trans.png")}
+              alt="Nickesalltjänst"
+            />
+          </a>
+        </div>
 
-          <div
-            id="mobile"
-            onClick={() => {
-              this.handleClick();
-            }}
-          >
-            <i
-              id="bar"
-              className={this.state.clicked ? "fas fa-times " : "fas fa-bars"}
-            ></i>
-          </div>
-        </nav>
-      </header>
-    );
-  }
+        <div className={clicked ? "dark-overlay" : "light-overlay"}>
+          <ul id="navbar" className={clicked ? "#navbar active" : "#navbar"}>
+            <li>
+              <a className="active hover-underline" href="/hem">
+                Hem
+              </a>
+            </li>
+            <li>
+              <a className="hover-underline" href="/nyheter">
+                Nyheter
+              </a>
+            </li>
+            <li>
+              <a className="hover-underline" href="/om">
+                Om
+              </a>
+            </li>
+            <li>
+              <a className="hover-underline" href="/kontakt">
+                Kontakt
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div
+          id="mobile"
+          onClick={() => {
+            handleClick();
+          }}
+        >
+          <i id="bar" className={clicked ? "fas fa-times " : "fas fa-bars"}></i>
+        </div>
+      </nav>
+    </header>
+  );
 }
-
-export default Navbar;
