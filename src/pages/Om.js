28317c7f0e682ee3a1f40b17113758ea4b7d0cc9
Linus <linusigelstrom@gmail.com>
2024-05-08 21:20:49 +0200

    started about page

--- src/pages/Om.js
+++ src/pages/Om.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import Hero from "../components/Hero";
 
 export default function About() {
-    return(
-        <div>
-            <h2>Om</h2>
-        </div>
-    )
+  return (
+    <div>
+      <Hero
+        text="Om oss"
+        bread="Nunc facilisis ut orci sit amet rhoncus. Quisque ullamcorper nisl eu sem lacinia pellentesque. Duis turpis risus, luctus vel tempus et, venenatis ultrices nibh. Phasellus consectetur dignissim sem, non venenatis nisi varius vel. Donec feugiat volutpat feugiat. Aenean tincidunt tellus ut convallis maximus. Proin cursus sit amet mi eget accumsan. Mauris laoreet tempor lacus, at venenatis odio ultrices vitae. Nam porta, arcu sit amet luctus volutpat, est elit cursus purus, eget rhoncus ipsum diam id justo. Phasellus pulvinar sed elit nec ullamcorper. Nulla a venenatis dolor."
+      />
+    </div>
+  );
 }
