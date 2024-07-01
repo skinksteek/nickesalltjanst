71edd4d32fa2efa13a82fc1f8aeb628fe357c201
Linus <linusigelstrom@gmail.com>
2024-07-01 21:11:12 +0200

    some last touches css and functionality

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Details from "./Details";
 import Inputs from "./Inputs";
 import "../assets/styles/contact/formpage.css";
 
 const ContactForm = () => {
+  const [messageSent, setMessageSent] = useState(false);
+
   return (
     <section className="page-wrapper">
       <div className="page-heading-wrapper">
-        <div className="contact-wrapper">
-          <Details />
-          <Inputs />
-        </div>
+        {!messageSent && (
+          <div className="contact-wrapper">
+            <Details />
+            <Inputs onMessageSent={setMessageSent} />
+          </div>
+        )}
+        {messageSent && (
+          <div className="message-box">
+            <p>Meddelande skickat!</p>
+            <button onClick={() => setMessageSent(false)}>Close</button>
+          </div>
+        )}
       </div>
     </section>
   );
--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import emailjs from "emailjs-com";
 import "../assets/styles/contact/form.css";
 import Button from "./Button";
 import { useNavigate } from "react-router-dom";
 
-const Inputs = () => {
+const Inputs = ({ onMessageSent }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -13,11 +14,20 @@ const Inputs = () => {
     message: "",
   });
 
-  const [formValid, setFormValid] = useState(false); // State to track form validation
-  const [phoneError, setPhoneError] = useState(""); // State for phone number error message
+  const [formValid, setFormValid] = useState(false);
+  const [phoneError, setPhoneError] = useState("");
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [submissionMessage, setSubmissionMessage] = useState("");
+  const [invalidFields, setInvalidFields] = useState({});
   const navigate = useNavigate();
 
-  // Function to handle changes in form fields
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setInvalidFields({});
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [invalidFields]);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -25,21 +35,16 @@ const Inputs = () => {
       [name]: value,
     });
 
-    // Validate form fields
     const {
       name: newName,
       email,
       phone,
       job,
       message,
-    } = {
-      ...formData,
-      [name]: value,
-    };
+    } = { ...formData, [name]: value };
     const isValid = newName && email && phone && job && message;
     setFormValid(isValid);
 
-    // Validate phone number format
     if (name === "phone") {
       const isValidPhone = /^[0-9]*$/.test(value);
       if (!isValidPhone) {
@@ -50,14 +55,26 @@ const Inputs = () => {
     }
   };
 
-  // Function to handle form submission
   const handleClick = (e) => {
     e.preventDefault();
 
-    // Check if phone number contains only numbers
     const isValidPhone = /^[0-9]*$/.test(formData.phone);
+    const newInvalidFields = {};
 
-    // If form is valid, proceed with sending email
+    if (!formData.name) newInvalidFields.name = true;
+    if (!formData.email) newInvalidFields.email = true;
+    if (!formData.phone) newInvalidFields.phone = true;
+    if (!formData.job) newInvalidFields.job = true;
+    if (!formData.message) newInvalidFields.message = true;
+
+    if (Object.keys(newInvalidFields).length > 0) {
+      setInvalidFields(newInvalidFields);
+      setSubmissionMessage(
+        "Fyll i alla obligatoriska fält innan du skickar formuläret."
+      );
+      return;
+    }
+
     if (formValid && isValidPhone) {
       emailjs
         .send(
@@ -68,11 +85,13 @@ const Inputs = () => {
         )
         .then(
           () => {
-            alert("Meddelande skickat!");
-            navigate("/"); // Redirect to the home page
+            setFormSubmitted(true);
+            setSubmissionMessage("Meddelande skickat!");
+            onMessageSent(true);
           },
           () => {
-            alert(
+            setFormSubmitted(true);
+            setSubmissionMessage(
               "Skicka meddelande misslyckades, fyllde du i allt du skulle?"
             );
           }
@@ -81,128 +100,166 @@ const Inputs = () => {
       if (!isValidPhone) {
         setPhoneError("Du kan enbart skriva siffror.");
       }
-      alert("Fyll i alla obligatoriska fält innan du skickar formuläret.");
+      setSubmissionMessage(
+        "Fyll i alla obligatoriska fält innan du skickar formuläret."
+      );
     }
   };
 
   return (
-    <form>
-      <div className="form__group field">
-        <input
-          type="text"
-          className="form__field"
-          placeholder="Namn"
-          name="name"
-          id="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="name" className="form__label">
-          Namn
-        </label>
-      </div>
-      <div className="form__group field">
-        <input
-          type="email"
-          className="form__field"
-          placeholder="E-post"
-          name="email"
-          id="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="email" className="form__label">
-          E-post
-        </label>
-      </div>
-      <div className="form__group field">
-        <input
-          type="tel"
-          className="form__field"
-          placeholder="Telefonnummer"
-          name="phone"
-          id="phone"
-          value={formData.phone}
-          onChange={handleChange}
-          pattern="[0-9]*" // Accepts only numeric input
-          required
-          title="Du kan enbart skriva siffror." // Error message for invalid input
-        />
-        {phoneError && <p className="error-message">{phoneError}</p>}
-        <label htmlFor="phone" className="form__label">
-          Telefonnummer
-        </label>
-      </div>
-      <div className="options-wrapper">
-        <div className="custom-select">
-          <select
-            name="job"
-            id="lang"
-            value={formData.job}
-            onChange={handleChange}
-            required
+    <div>
+      {!formSubmitted ? (
+        <form>
+          <div
+            className={`form__group field ${invalidFields.name ? "shake" : ""}`}
           >
-            <option value="" disabled selected>
-              Välj jobb
-            </option>
-            <option className="option-color" value="Vatten och avlopp">
-              Vatten och avlopp
-            </option>
-            <option className="option-color" value="Dränering">
-              Dränering
-            </option>
-            <option className="option-color" value="Plattsättning">
-              Plattsättning
-            </option>
-            <option className="option-color" value="Plantering och borttagning">
-              Plantering och borttagning
-            </option>
-            <option className="option-color" value="Mindre husgrunder">
-              Mindre husgrunder
-            </option>
-            <option className="option-color" value="Grustransporter">
-              Grustransporter
-            </option>
-            <option className="option-color" value="Skräp och riscontainer">
-              Skräp och riscontainer
-            </option>
-            <option
-              className="option-color"
-              value="Krantransporter med lastbil"
-            >
-              Krantransporter med lastbil
-            </option>
-            <option className="option-color" value="Bilreparationer">
-              Bilreparationer
-            </option>
-            <option className="option-color" value="Däckförsäljning">
-              Däckförsäljning
-            </option>
-            <option
-              className="option-color"
-              value="Mindre snickeri och byggprojekt"
-            >
-              Mindre snickeri och byggprojekt
-            </option>
-          </select>
+            <input
+              type="text"
+              className={`form__field ${
+                invalidFields.name ? "error-border" : ""
+              }`}
+              placeholder="Namn"
+              name="name"
+              id="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="name" className="form__label">
+              Namn
+            </label>
+          </div>
+          <div
+            className={`form__group field ${
+              invalidFields.email ? "shake" : ""
+            }`}
+          >
+            <input
+              type="email"
+              className={`form__field ${
+                invalidFields.email ? "error-border" : ""
+              }`}
+              placeholder="E-post"
+              name="email"
+              id="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="email" className="form__label">
+              E-post
+            </label>
+          </div>
+          <div
+            className={`form__group field ${
+              invalidFields.phone ? "shake" : ""
+            }`}
+          >
+            <input
+              type="tel"
+              className={`form__field ${
+                invalidFields.phone ? "error-border" : ""
+              }`}
+              placeholder="Telefonnummer"
+              name="phone"
+              id="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              pattern="[0-9]*"
+              required
+              title="Du kan enbart skriva siffror."
+            />
+            {phoneError && <p className="error-message">{phoneError}</p>}
+            <label htmlFor="phone" className="form__label">
+              Telefonnummer
+            </label>
+          </div>
+          <div
+            className={`options-wrapper ${invalidFields.job ? "shake" : ""}`}
+          >
+            <div className="custom-select">
+              <select
+                name="job"
+                id="lang"
+                className={`${invalidFields.job ? "error-border" : ""}`}
+                value={formData.job}
+                onChange={handleChange}
+                required
+              >
+                <option value="" disabled selected>
+                  Välj jobb
+                </option>
+                <option className="option-color" value="Vatten och avlopp">
+                  Vatten och avlopp
+                </option>
+                <option className="option-color" value="Dränering">
+                  Dränering
+                </option>
+                <option className="option-color" value="Plattsättning">
+                  Plattsättning
+                </option>
+                <option
+                  className="option-color"
+                  value="Plantering och borttagning"
+                >
+                  Plantering och borttagning
+                </option>
+                <option className="option-color" value="Mindre husgrunder">
+                  Mindre husgrunder
+                </option>
+                <option className="option-color" value="Grustransporter">
+                  Grustransporter
+                </option>
+                <option className="option-color" value="Skräp och riscontainer">
+                  Skräp och riscontainer
+                </option>
+                <option
+                  className="option-color"
+                  value="Krantransporter med lastbil"
+                >
+                  Krantransporter med lastbil
+                </option>
+                <option className="option-color" value="Bilreparationer">
+                  Bilreparationer
+                </option>
+                <option className="option-color" value="Däckförsäljning">
+                  Däckförsäljning
+                </option>
+                <option
+                  className="option-color"
+                  value="Mindre snickeri och byggprojekt"
+                >
+                  Mindre snickeri och byggprojekt
+                </option>
+              </select>
+            </div>
+            <textarea
+              placeholder="Övrig information"
+              cols="50"
+              rows="10"
+              className={`${invalidFields.message ? "error-border" : ""}`}
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="button-holder">
+            <Button text="Skicka" onClick={handleClick} disabled={!formValid} />
+          </div>
+        </form>
+      ) : (
+        <div className="message-box">
+          <p>{submissionMessage}</p>
+          <button onClick={() => navigate("/")}>Stäng</button>
         </div>
-        <textarea
-          placeholder="Övrig information"
-          cols="50"
-          rows="10"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="button-holder">
-        <Button text="Skicka" onClick={handleClick} disabled={!formValid} />
-      </div>
-    </form>
+      )}
+    </div>
   );
+};
+
+Inputs.propTypes = {
+  onMessageSent: PropTypes.func.isRequired,
 };
 
 export default Inputs;
