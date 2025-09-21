406787a460901f22a90686706951844acf18b981
skinksteek <linusigelstrom@gmail.com>
2025-09-21 18:06:53 +0200

    hero to contact page

--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -1,9 +1,10 @@
-import ContactForm from "../components/ContactForm";
+import ContactForm from "../components/contact/ContactForm";
 import Hero from "../components/Hero";
 
 export default function Contact() {
   return (
     <div>
+      <Hero text="Kontakta oss" />
       <ContactForm />
     </div>
   );
