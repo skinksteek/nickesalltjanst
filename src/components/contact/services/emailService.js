ffaabeb5bd874672c9bbffad81e46f3eb81619f4
skinksteek <linusigelstrom@gmail.com>
2025-12-01 16:57:08 +0100

    emailjs fix

--- src/components/contact/services/emailService.js
+++ src/components/contact/services/emailService.js
@@ -1,8 +1,11 @@
 import emailjs from "@emailjs/browser";
 
-const SERVICE_ID = import.meta.env.SERVICE_ID;
-const TEMPLATE_ID = import.meta.env.TEMPLATE_ID;
-const PUBLIC_KEY = import.meta.env.PUBLIC_KEY;
+const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
+const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
+const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
+console.log("SERVICE_ID:", SERVICE_ID);
+console.log("TEMPLATE_ID:", TEMPLATE_ID);
+console.log("PUBLIC_KEY:", PUBLIC_KEY);
 
 export async function sendContact(formData) {
   return emailjs.send(SERVICE_ID, TEMPLATE_ID, formData, PUBLIC_KEY);
